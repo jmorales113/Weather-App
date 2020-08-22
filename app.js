e5297e31a4474775ce26de75d40485954166e6c8
jmorales113 <jmorales113@gmail.com>
2020-08-22 16:11:25 -0700

    Set up error handling

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const request = require("request")
 const weatherUrl = "http://api.weatherstack.com/current?access_key=ccc2c04a37c1c3342d8da4cc9b44ce31&query=37.8267,-122.4233&units=f";
 
 request({ url: weatherUrl, json: true }, (error, response) => {
-    // console.log(response.body.current)
-    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+
+    if (error) {
+        console.log("Unable to connect to weather service")
+    } else if (response.body.error) {
+        console.log("Unable to find location")
+    } else {
+         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+    }
 })
 
-const geocodeURL ="https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoiam1vcmFsZXMxMTMiLCJhIjoiY2tkejd5aDVqMDdlMTJ6cDc0Mjloc204cyJ9.LyltzXMQZd7EfUY515UbqA&limit=1"
+const geocodeURL ="https://api.mapbox.com/geocoding/v5/mapbox.places/losangeles.json?access_token=pk.eyJ1Ijoiam1vcmFsZXMxMTMiLCJhIjoiY2tkejd5aDVqMDdlMTJ6cDc0Mjloc204cyJ9.LyltzXMQZd7EfUY515UbqA&limit=1"
 
-request( { url: geocodeURL, json: true}, (error, response) => {
-    console.log(response.body.features[0].center)
+request({ url: geocodeURL, json: true}, (error, response) => {
+
+    if (error) {
+        console.log("Unable to connect to find geolocation service")
+    } else if (response.body.features.length < 1) {
+        console.log("Unable to find geolocation. Try another search.")
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 })
