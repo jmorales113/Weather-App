bb3572d5f701649ee47937d44c4b2deac172bf4b
jmorales113 <jmorales113@gmail.com>
2020-08-25 16:33:57 -0700

    Fixed url error

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require("request")
 
 const forecast = (latitude, longitude, callback) => {
-    const forecastURL = `http://api.weatherstack.com/current?access_key=ccc2c04a37c1c3342d8da4cc9b44ce31&query=${latitude},${longitude}&units=f`
-    request({ url: forecastURL, json: true }, (error, response) => {
+    const url = `http://api.weatherstack.com/current?access_key=ccc2c04a37c1c3342d8da4cc9b44ce31&query=${latitude},${longitude}&units=f`
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to weather service!", undefined)
         }
-        else if (response.body.error) {
+        else if (body.error) {
             callback("Unable to find location!", undefined)
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
         }
     })
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,19 +1,19 @@
 const request = require("request")
 
 const geocode = (address, callback) => {
-    const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoiam1vcmFsZXMxMTMiLCJhIjoiY2tkejd5aDVqMDdlMTJ6cDc0Mjloc204cyJ9.LyltzXMQZd7EfUY515UbqA&limit=1`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoiam1vcmFsZXMxMTMiLCJhIjoiY2tkejd5aDVqMDdlMTJ6cDc0Mjloc204cyJ9.LyltzXMQZd7EfUY515UbqA&limit=1`
 
-    request({ url: geocodeUrl, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to location services!", undefined)
         }
-        else if (response.body.features.length < 1) {
+        else if (body.features.length < 1) {
             callback("Unable to find location. Try another search", undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
