bb4a1f757dfd9d8157ddba40883383c3f8f354ac
jmorales113 <jmorales113@gmail.com>
2020-08-30 14:45:40 -0700

    Setup dynamic page rendering with hbs, handlebars and express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const path = require("path")
 const app = express()
 const publicDirectory = path.join(__dirname, "../public")
 
+app.set("view engine", "hbs")
 app.use(express.static(publicDirectory))
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather",
+        name: "Jorge Morales"
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me",
+        name: "Jorge Morales"
+    })
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help page",
+        help: "This is the help page."
+    })
+})
 
 app.get("/weather", (req, res) => {
     res.send({
