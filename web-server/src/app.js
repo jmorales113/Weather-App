ce2bfb46bfdcdf3555db85a5bafc146ecfbd6e7c
jmorales113 <jmorales113@gmail.com>
2020-09-02 16:08:40 -0700

    Configured partials through Express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const path = require("path")
+const hbs = require("hbs")
 
 const app = express()
 
 // Define paths for Express confit
 const publicDirectory = path.join(__dirname, "../public")
-const viewsPath = path.join(__dirname, "../templates")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs")
 app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectory))
