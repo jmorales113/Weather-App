822ba72381a67a04f508bcaf3f9b50b1788544ff
jmorales113 <jmorales113@gmail.com>
2020-09-02 17:53:51 -0700

    Rendered 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,22 @@ app.get("/weather", (req, res) => {
     })
 })
 
+app.get("/help/*", (req, res) => {
+    res.render("404",{
+        title: "404",
+        name: "Jorge Morales",
+        error: "Help article not found."
+    })
+})
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: "Jorge Morales",
+        error: "Page not found."
+    })
+})
+
 app.listen(3000, () => {
     console.log("Server is up on Port 3000.")
 })
