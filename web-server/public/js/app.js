2cee95c49dbee9b96f55e7ada56468c67e6dc274
jmorales113 <jmorales113@gmail.com>
2020-09-12 11:41:38 -0700

    Added some more styling

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,27 @@
 console.log("Client side javascript file is loaded!")
+
+const weatherForm = document.querySelector("form")
+const searchInput = document.querySelector("input")
+const weatherMessage = document.querySelector("#weather-message")
+const locationMessage = document.querySelector("#location-message")
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const searchLocation = searchInput.value
+
+    weatherMessage.textContent = "Loading weather..."
+    locationMessage.textContent = ""
+
+    fetch(`http://localhost:3000/weather?address=${searchLocation}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                weatherMessage.textContent = data.error
+            } else {
+                weatherMessage.textContent = data.forecast
+                locationMessage.textContent = data.location
+            }
+        })
+    })
+})
+
